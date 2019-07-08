e134042dfdec1282a188c601398920e20b61db3f
johncook <jonnymcook1@gmail.com>
2019-07-08 15:30:01 -0500

    biuld ready

--- db/get_show_artist.sql
+++ db/get_show_artist.sql
@@ -3,5 +3,5 @@ JOIN artist
 ON artist.artist_id = event.artist_id
 JOIN users
 ON users.user_id = artist.users_id
- WHERE accepted = TRUE
- AND artist.artist_id = $1
+WHERE accepted = TRUE
+AND artist.artist_id = $1
